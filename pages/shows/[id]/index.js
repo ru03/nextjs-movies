aaea01b7a2f8dac4472bb05d84fb415b0ba0b168
Rubén <ruben.lmangas@gmail.com>
2020-03-15 19:09:36 +0100

    Improve show detail page

--- pages/shows/[id]/index.js
+++ pages/shows/[id]/index.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 
 const Id = ({ show }) => {
+  console.log(show)
   return (
     <div className="container mx-auto">
       <div className="flex flex-row justify-around mt-10">
@@ -11,10 +12,16 @@ const Id = ({ show }) => {
           <div className="text-5xl">
             {show.name} <span className="text-base font-thin text-gray-500">({show.premiered})</span>
           </div>
-          <div className="text-sm text-gray-600">
-            {show.genres.join(', ')} | {show.language}
+          <div className="flex justify-between">
+            <div className="text-sm text-gray-600">
+              {show.genres.join(', ')} | {show.language}
+            </div>
+            <div className="text-base font-bold">
+              <i className="fas fa-star px-1 text-yellow-500" />
+              {show.rating.average} / 10
+            </div>
           </div>
-          <div className="w-full h-1 my-5 bg-color bg-gray-700" />
+          <div className="w-full h-px my-1 bg-color bg-gray-700" />
           <div className="text-base" dangerouslySetInnerHTML={{ __html: show.summary }} />
         </div>
       </div>
@@ -24,7 +31,7 @@ const Id = ({ show }) => {
 
 export async function getServerSideProps(context) {
   const id = context.params.id;
-  const res = await fetch(`http://api.tvmaze.com/shows/${id}`);
+  const res = await fetch(`http://api.tvmaze.com/shows/${id}?embed=seasons`);
   const show = await res.json();
   return {
     props: {
